8d14d4572fb5d0fbbb745107ec12e8d83c7f08cc
Abraham Romero <romero9804@hotmail.com>
2024-12-20 12:32:31 -0800

    botones tags

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,7 +1,38 @@
+import styled from "styled-components"
+import tags from "./tags.json"
+
+const TagsContainer = styled.div`
+    display: flex;
+    gap: 24px;
+`
+
+const ButtonTag = styled.button`
+    background-color: #94A3B8;
+    border-radius: 10px;
+    padding: 8px 10px;
+    font-size: 24px;
+    color: white;
+    border: 2px solid #94A3B8; 
+    box-sizing: border-box;
+    &:hover{
+        border: 2px solid #C98CF1;
+    }
+`
+
+const TagTitulo = styled.h3`
+    color: white;
+    font-size: 24px;
+`
+
+
 const Tags = () => {
-    return <p>
-        Tags
-    </p>
+    return <TagsContainer>
+        <TagTitulo>Busque por tags: </TagTitulo>
+        {tags.map(tag => {
+            return <ButtonTag id={tag.id}>{tag.titulo}</ButtonTag>
+        })}
+    </TagsContainer>
+        
 }
 
 export default Tags
